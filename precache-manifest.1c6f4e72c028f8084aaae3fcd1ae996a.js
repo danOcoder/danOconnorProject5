10a200f59de38460bafd87bdd55cc2fc8d9d9d53
Dan <dan_oconnor@live.ca>
2019-04-29 12:34:26 -0400

    Updates

---
+++ precache-manifest.1c6f4e72c028f8084aaae3fcd1ae996a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "899e2ddf37a2811ac7b9ed2b8055e09a",
+    "url": "/potus-quotes/static/media/potus.899e2ddf.png"
+  },
+  {
+    "revision": "2ffc1d575eadb712aa37468d9aceb4cd",
+    "url": "/potus-quotes/static/media/header.2ffc1d57.png"
+  },
+  {
+    "revision": "6c3818293f5d1990bf269dfe5d1c6bfd",
+    "url": "/potus-quotes/static/media/footer.6c381829.png"
+  },
+  {
+    "revision": "9499034de8fea5f2f294",
+    "url": "/potus-quotes/static/js/runtime~main.9499034d.js"
+  },
+  {
+    "revision": "8c0cb10d7b2e46748dbf",
+    "url": "/potus-quotes/static/js/main.8c0cb10d.chunk.js"
+  },
+  {
+    "revision": "3837abb3f45c28aae9c2",
+    "url": "/potus-quotes/static/js/2.3837abb3.chunk.js"
+  },
+  {
+    "revision": "8c0cb10d7b2e46748dbf",
+    "url": "/potus-quotes/static/css/main.0059a807.chunk.css"
+  },
+  {
+    "revision": "878b3fcd957039097ed69657087d5c32",
+    "url": "/potus-quotes/index.html"
+  }
+];
